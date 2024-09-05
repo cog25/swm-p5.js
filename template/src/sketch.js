fdb272d1ec8172ac48ccaa36ae0cf29897e7c7e3
cog25 <74242561+cog25@users.noreply.github.com>
2024-09-05 14:26:03 +0000

    실습용 Template 만들깅..

---
+++ template/src/sketch.js
@@ -0,0 +1,13 @@
+export function setup() {
+  createCanvas(400, 400);
+}
+
+export function draw() {
+  background("#f2f2f2");
+
+  stroke(0,0,0);
+  fill(255,255,255);
+  
+  circle(width/2, height/2, 100);
+}
+
